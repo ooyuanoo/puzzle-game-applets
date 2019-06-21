79847f1b96a099b24771c822ad0608d2169ab2a1
yuanqiujuan <yuanqiujuan@bigo.sg>
2019-06-21 17:54:47 +0800

    修改布局

--- pages/games/game-puzzle/index.js
+++ pages/games/game-puzzle/index.js
@@ -7,9 +7,14 @@ Page({
   data: {
     canvasWidth: 0,
     dots: [],
+    emptyDot: {
+      x: 0,
+      y: 0
+    },
     ctx: null,
     imgUrl: '',
-    slice: 9 // 默认分割的片数
+    slice: 9, // 默认分割的片数
+    perWidth: ''
   },
 
   /**
@@ -76,49 +81,93 @@ Page({
 
   },
 
+  loopSlice: function(callback) {
+    const perLineNum = Math.sqrt(this.data.slice);
+    this.data.perWidth = this.data.canvasWidth / perLineNum;
+
+    // 保存正确的点
+    for (let i = 0; i < perLineNum; i++) {
+      for (let j = 0; j < perLineNum; j++) {
+        callback && callback(i, j);
+      }
+    }
+  },
+
   drawImageCanvas: function (src) {
     wx.getImageInfo({
       src: src,
       success: (res) => {
         this.data.imgUrl = src;
-        this.spliceCardImg()
+        this.drawGamePanel();
       }
     })
   },
 
-  spliceCardImg: function () {
-    const lineNum = Math.sqrt(this.data.slice);
-    const singleWidth = this.data.canvasWidth / lineNum;
+  drawGamePanel: function() {
+    // 保存正确的点
+    this.loopSlice((i, j) => {
+      this.data.dots.push({
+        x: i * this.data.perWidth,
+        y: j * this.data.perWidth,
+        position: [i, j]
+      });
+    });
 
-    for (let i = 0; i < lineNum; i++) {
-      for (let j = 0; j < lineNum; j++) {
-        this.data.dots.push({
-          x: i * singleWidth,
-          y: j * singleWidth
-        });
-      }
-    }
-
-    let drawDots = this.data.dots.slice(0, this.data.dots.length - 1);
-
-    drawDots = drawDots.sort((a, b) => {
+    // 获取n-1个碎片，并且打乱顺序
+    let drawDots = this.data.dots.slice(0, this.data.dots.length - 1).sort((a, b) => {
       return 0.5 - Math.random();
-    })
+    });
 
-    drawDots.forEach((o) => {
-      this.drawGameCard(o, singleWidth)
-    })
+    // 记录一个空的节点
+    this.data.emptyDot.x = this.data.dots[this.data.dots.length - 1].x;
+    this.data.emptyDot.y = this.data.dots[this.data.dots.length - 1].y;
 
-    this.data.ctx.draw();
+    // 循环画画
+    this.drawGameCard(drawDots);
   },
 
-  drawGameCard(item, singleWidth) {
+  drawGameCard: function (drawDots) {
+    const perLineNum = Math.sqrt(this.data.slice);
     const splitLineWidth = 3;
+    let count = 0;
 
-    this.data.ctx.drawImage(this.data.imgUrl, item.x, item.y, singleWidth, singleWidth, item.x, item.y, singleWidth, singleWidth);
-    this.data.ctx.setLineWidth(splitLineWidth);
-    this.data.ctx.setStrokeStyle('#56A902');
-    this.data.ctx.strokeRect(item.x, item.y, singleWidth, singleWidth);
+    this.loopSlice((i, j) => {
+      if (i === perLineNum - 1 && j === perLineNum - 1) {
+        return;
+      }
+
+      this.data.ctx.drawImage(
+        this.data.imgUrl,
+        drawDots[count].x,
+        drawDots[count].y,
+        this.data.perWidth,
+        this.data.perWidth,
+        i * this.data.perWidth,
+        j * this.data.perWidth,
+        this.data.perWidth,
+        this.data.perWidth
+      );
+      this.data.ctx.setLineWidth(splitLineWidth);
+      this.data.ctx.setStrokeStyle('#56A902');
+      this.data.ctx.strokeRect(i * perWidth, j * perWidth, perWidth, perWidth);
+      this.data.ctx.draw(true);
+      count++;
+    });
+  },
+
+  slideCard: function(e) {
+    let count = 0;
+    let matchedIndex = 0;
+    let {x, y} = e.changedTouches[0];
+
+    this.loopSlice((i, j) => {
+      if (x > (i - 1) * this.data.perWidth && x < i * this.data.perWidth &&
+        y > (j - 1) * this.data.perWidth && y < j * this.data.perWidth) {
+
+      }
+
+      count++;
+    })
   },
 
   canvasIdErrorCallback: function (e) {
